85c349872eb9b7024f1a3428c4830da438533645
MayYuSan <109531503+mayyusan101@users.noreply.github.com>
2024-01-13 17:25:13 +0630

    Update server.js

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ const cors = require("cors");
 
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: "https://chat-app-frontend-xmwz.vercel.app" } });
+const io = new Server(httpServer, { cors: { origin: "https://chat-app-frontend-xmwz-lzhvnhdlt-mayyusan101.vercel.app" } });
 
 // Enable CORS for all routes
-// app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://chat-app-frontend-xmwz.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'https://chat-app-frontend-xmwz.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// });
 
 const PORT = process.env.PORT || 5000;
 const dbConnect = require("./config/db");
